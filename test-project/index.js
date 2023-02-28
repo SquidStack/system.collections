e3a74fe8e9d39240edbceef772a098b23df49fc3
Chris Eadie <caeadie@hotmail.com>
2023-02-28 20:30:43 +0000

    final package stuff working

--- test-project/index.js
+++ test-project/index.js
@@ -1,1 +1,7 @@
-import {Iterator,Dictionary,LinkedList, List} from 'system.collections'
+import {List} from '@squidstack/system.collections'
+
+var ls = new List();
+
+ls.add(1);
+
+console.log(ls.toArray())
